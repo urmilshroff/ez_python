4fb16bdd79cb9df113d6f254995d198636fe9131
Urmil Shroff <urmilshroff@gmail.com>
2017-10-17 12:38:51 +0530

    Readded everything

---
+++ brains.js
@@ -0,0 +1,14 @@
+function logo_click()
+{
+window.open("https://www.python.org");
+}
+
+/*function mouse_over()
+{
+document.write("onmouseover() called");
+}*/
+
+function button_click()
+{
+document.write("A button was clicked");
+}
