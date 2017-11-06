4f9e77822b947df27397a9ad7a3bc04905a52e81
Urmil Shroff <urmilshroff@gmail.com>
2017-11-06 20:51:28 +0530

    Started working on account creation page

--- js/brain.js
+++ js/brain.js
@@ -3,6 +3,11 @@ function python_org()
 window.open("https://www.python.org");
 }
 
+function execute_online()
+{
+window.open("https://www.tutorialspoint.com/execute_python_online.php");
+}
+
 function mouse_over()
 {
 document.write("onmouseover() called");
@@ -11,4 +16,17 @@ document.write("onmouseover() called");
 function button_click()
 {
 document.write("A button was clicked");
+}
+
+function account_validator()
+{
+
+var fname=document.account.fname;
+var sname=document.account.sname;
+var username=document.account.username;
+var email=document.account.email;
+var password=document.account.password;
+
+
+
 }
